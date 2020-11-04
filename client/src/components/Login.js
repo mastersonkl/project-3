c49907d779d9eb5fc078635d4f66392eba2e857d
Kendall Masterson <masterson.kendall@gmail.com>
2020-11-03 22:19:09 -0500

    User stays logged in with page refresh

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,79 +1,90 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 
 function Login() {
-  const [usernameReg, setUsernameReg] = useState("");
-  const [passwordReg, setPasswordReg] = useState("");
+    const [usernameReg, setUsernameReg] = useState("");
+    const [passwordReg, setPasswordReg] = useState("");
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-  const [loginStatus, setLoginStatus] = useState("");
+    const [loginStatus, setLoginStatus] = useState("");
 
-  const register = () => {
-    Axios.post("/register", {
-      username: usernameReg,
-      password: passwordReg,
-    }).then((response) => {
-      console.log(response);
-    });
-  };
+    Axios.defaults.withCredentials = true;
 
-  const login = () => {
-    Axios.post("/login", {
-      username: username,
-      password: password,
-    }).then((response) => {
-      if (response.data.message) {
-        setLoginStatus(response.data.message);
-      } else {
-        setLoginStatus(response.data[0].username);
-      }
-    });
-  };
+    const register = () => {
+        Axios.post("/register", {
+            username: usernameReg,
+            password: passwordReg,
+        }).then((response) => {
+            console.log(response);
+        });
+    };
 
-  return (
-    <div className="register">
-      <div className="registration">
-        <h1>Registration</h1>
-        <label>Username</label>
-        <input
-          type="text"
-          onChange={(e) => {
-            setUsernameReg(e.target.value);
-          }}
-        />
-        <label>Password</label>
-        <input
-          type="text"
-          onChange={(e) => {
-            setPasswordReg(e.target.value);
-          }}
-        />
-        <button onClick={register}>Register</button>
-      </div>
+    const login = () => {
+        Axios.post("/login", {
+            username: username,
+            password: password,
+        }).then((response) => {
+            if (response.data.message) {
+                setLoginStatus(response.data.message);
+            } else {
+                setLoginStatus(response.data[0].username);
+            }
+        });
+    };
 
-      <div className="login">
-        <h1>Login</h1>
-        <input
-          type="text"
-          placeholder="Username..."
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          placeholder="Password..."
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <button onClick={login}>Login</button>
-      </div>
-      <h1>{loginStatus}</h1>
-    </div>
-  );
+    useEffect(() => {
+        Axios.get("/login").then((response) => {
+            if (response.data.loggedIn == true) {
+                setLoginStatus(response.data.user[0].username);
+            }
+        });
+    }, []);
+
+
+    return (
+        <div className="register">
+            <div className="registration">
+                <h1>Registration</h1>
+                <label>Username</label>
+                <input
+                    type="text"
+                    onChange={(e) => {
+                        setUsernameReg(e.target.value);
+                    }}
+                />
+                <label>Password</label>
+                <input
+                    type="text"
+                    onChange={(e) => {
+                        setPasswordReg(e.target.value);
+                    }}
+                />
+                <button onClick={register}>Register</button>
+            </div>
+
+            <div className="login">
+                <h1>Login</h1>
+                <input
+                    type="text"
+                    placeholder="Username..."
+                    onChange={(e) => {
+                        setUsername(e.target.value);
+                    }}
+                />
+                <input
+                    type="password"
+                    placeholder="Password..."
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                />
+                <button onClick={login}>Login</button>
+            </div>
+            <h1>{loginStatus}</h1>
+        </div>
+    );
 }
 
 export default Login;
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const mysql = require("mysql");
 const cors = require("cors");
 
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
@@ -9,9 +12,23 @@ const saltRounds = 10;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use(session({
+  key: "userId",
+  secret: "subscribe",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 60 * 60 * 24
+  },
+}))
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -42,6 +59,16 @@ app.post("/register", (req, res) => {
   })
 });
 
+
+app.get("/login", (req, res) => {
+  if (req.session.user) {
+    res.send({ loggedIn: true, user: req.session.user });
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
+
+
 app.post("/login", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -57,7 +84,9 @@ app.post("/login", (req, res) => {
       if (result.length > 0) {
         bcrypt.compare(password, result[0].password, (error, response) => {
           if (response) {
-          res.send(result)
+            req.session.user = result;
+            console.log(req.session.user);
+            res.send(result);
           } else {
             res.send({ message: "Wrong username and password combination" });
           }
