cefa1522932e18986829c3fb22c51f93ed723668
honey bee <honeybee@users.noreply.github.com>
2020-11-06 18:49:55 -0500

    added lots of fancy styling to the form

--- client/src/components/Rating.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Rating } from 'semantic-ui-react'
-
-const RatingStar = () => (
-  <Rating icon='star' size='large' defaultRating={3} maxRating={4} />
-)
-
-export default RatingStar
